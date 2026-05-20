787b8ad9460d956291e061f11ee7a6d18c3836be
Andy S Alic <>
2026-05-20 13:48:18 +0200

    remove the basename from the URLs

--- src/service/UrlFactory.ts
+++ src/service/UrlFactory.ts
@@ -11,60 +11,50 @@ import UrlTags from "../model/UrlTags";
 export default class UrlFactory {
 
     static baseName = config.basename.endsWith("/") ? config.basename.substring(0, config.basename.length - 1) : config.basename;
-    //static base = `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ""}${UrlFactory.base()}`;
+    //static base = `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ""}`;
 
     public static base() {
         return UrlFactory.baseName;
     }
     public static singleData(singleDataType: SingleDataType, queryParams?: UrlTags): string { 
-        return `${UrlFactory.base()}/${Util.singleDataPath(singleDataType)}${UrlFactory.queryParams(queryParams)}`;
+        return `/${Util.singleDataPath(singleDataType)}${UrlFactory.queryParams(queryParams)}`;
     }
     public static singleDataDetails(id: string, singleDataType: SingleDataType): string {
         return `${UrlFactory.singleData(singleDataType)}/${id}/details`
     }
 
-    public static sites(): string { return `${UrlFactory.base()}/sites`}
+    public static sites(): string { return `/sites`}
 
-    public static getPutSite(siteCode: string): string { return `${UrlFactory.base()}/sites/${siteCode}`;}
+    public static getPutSite(siteCode: string): string { return `/sites/${siteCode}`;}
 
-    public static siteCreator(): string { return `${UrlFactory.base()}/sites/creator`; }
+    public static siteCreator(): string { return `/sites/creator`; }
 
     public static projectDetails(code: string): string {
-        return `${UrlFactory.base()}/projects/${code}/details`
+        return `/projects/${code}/details`
     }
 
     public static projectNew():  string {
-        return `${UrlFactory.base()}/projects/new`;
+        return `/projects/new`;
     }
 
     public static projects(): string {
-        return `${UrlFactory.base()}/projects`;
+        return `/projects`;
     }
 
     public static subprojectEditor(code: string, subcode: string): string 
-        {return `${UrlFactory.base()}/projects/${code}/subprojects/${subcode}/editor`;}
+        {return `/projects/${code}/subprojects/${subcode}/editor`;}
 
     public static subprojectNew(code: string): string 
-        {return `${UrlFactory.base()}/projects/${code}/subprojects/new`;}
+        {return `/projects/${code}/subprojects/new`;}
 
     public static projectConfigEdit(code: string): string {
-        return `${UrlFactory.base()}/projects/${code}/config-editor`;
+        return `/projects/${code}/config-editor`;
     }
 
-    public static projectLogoFullUrl(logoUrl: string): string {
-        return `${config.datasetService.projectLogo}${logoUrl}`;
-        // const url: URL | null = URL.parse(Config.datasetService.api);
-        // if (url) {
-        //     return `${url.protocol}//${url.hostname}${url.port ? `:${url.port}` : ""}${Config.base()}${logoUrl}`
-        // } else {
-        //     return `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ""}${Config.base()}${logoUrl}`
-        // }
-    }
-
-    public static users(): string { return `${UrlFactory.base()}/users`;}
+    public static users(): string { return `/users`;}
 
     public static userBase(username: string): string {
-        return `${UrlFactory.base()}/users/${username}`;
+        return `/users/${username}`;
     }
     public static userDetails(username: string): string {
         return `${this.userBase(username)}/${UserView.TAB_DETAILS}`;
@@ -73,9 +63,9 @@ export default class UrlFactory {
         return `${this.userBase(username)}/${UserView.TAB_LOGS}`;
     }
 
-    public static fair(): string { return `${UrlFactory.base()}/fair`; }
+    public static fair(): string { return `/fair`; }
 
-    public static support(): string { return `${UrlFactory.base()}/support`; }
+    public static support(): string { return `/support`; }
 
     public static queryParams(qps?: UrlTags): string {
         if (qps) { 
